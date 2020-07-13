27ec44406720aaae8f52ab344458038c234ea072
Alexis <alexisdkreager@gmail.com>
2020-07-13 09:25:50 -0500

    Create AllEqual.java

---
+++ src/main/java/com/galvanize/AllEqual.java
@@ -0,0 +1,15 @@
+package com.galvanize;
+
+public class AllEqual {
+
+    Boolean allEqual(String input) {
+        // write real code
+        return false;
+    }
+
+    public static void main(String[] args) {
+        AllEqual object = new AllEqual();
+        System.out.println(object.allEqual("blahblahblah"));
+    }
+
+}
