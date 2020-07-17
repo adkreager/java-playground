076cd270b7abcca66dbe34fecb1068759292b259
Alexis <alexisdkreager@gmail.com>
2020-07-17 14:46:07 -0500

    Update Application.java

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,8 +1,39 @@
 package com.galvanize;
 
 public class Application {
+
+//    boolean isActive(String status) {
+//        if (status.equals("active")) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+
+    char grade(int input) {
+        if (input >= 90) {
+            return 'A';
+        } else if (input >= 80) {
+            return 'B';
+        } else if (input >= 70) {
+            return 'C';
+        } else if (input >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+
+
     public static void main(String[] args) {
+        Application template = new Application();
         System.out.println("hello world");
+//        System.out.println(isActive("Active"));
+//        System.out.println(isActive("active"));
+        System.out.println(template.grade(Integer.parseInt(args[0])));
+
+        System.out.println();
+
 
     }
 
